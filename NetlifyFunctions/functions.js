5ac55f06aad87d61118df1fec276566774dc895d
Daniel W. R. Hart <30510653+Daniel-WR-Hart@users.noreply.github.com>
2020-12-15 18:05:31 -0700

    Update functions.js

--- NetlifyFunctions/functions.js
+++ NetlifyFunctions/functions.js
@@ -1,9 +1,17 @@
-require('dotenv').config();
-const { apiKey } = process.env;
+// require('dotenv').config();
+// const { apiKey } = process.env;
 
-exports.handler = async (event, context) => {
+// exports.handler = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: apiKey
+//   };
+// };
+
+
+exports.handler = async () => {
   return {
     statusCode: 200,
-    body: apiKey
+    body: "hello"
   };
 };
