ef647a19cc373b5335c2a88c8d77c2f348fd3b2d
Daniel W. R. Hart <30510653+Daniel-WR-Hart@users.noreply.github.com>
2020-12-15 19:15:01 -0700

    Create getApiKey.js

---
+++ functions/getApiKey.js
@@ -0,0 +1,25 @@
+// require('dotenv').config();
+// const { apiKey } = process.env;
+
+// exports.handler = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: apiKey
+//   };
+// };
+
+
+// exports.handler = () => {
+//   return {
+//     statusCode: 200,
+//     body: "hello"
+//   };
+// };
+
+
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: "hello"
+  });
+}
